c37bafd67dcca582bb91840cc2069cfdec0dd95d
SaluSL <47835218+SaluSL@users.noreply.github.com>
2021-01-17 17:49:38 +0100

    fixed connection problems

--- server.c
+++ server.c
@@ -16,6 +16,8 @@ int main() {
 
     socklen_t sin_size;
 
+    int counter, received;
+
     struct sockaddr_in incoming;
 
     struct hostent *heLocalHost;
@@ -32,34 +34,57 @@ int main() {
 
     endpoint.sin_family = AF_INET;
     endpoint.sin_port = htons(2137);
-    endpoint.sin_addr = *(struct in_addr*)heLocalHost ->h_addr;
+    endpoint.sin_addr.s_addr = INADDR_ANY;
 
-    memset(&(endpoint.sin_zero),0,8);
+    memset(&(endpoint.sin_zero), 0, 8);
 
 
-    bindSock = bind(sdServerSocket, (struct sockaddr*) &endpoint, sizeof(struct sockaddr));
+    bindSock = bind(sdServerSocket, (struct sockaddr *) &endpoint, sizeof(struct sockaddr));
 
-    if (bindSock < 0) { printf("bind nie powiodl sie");return 1;}
+    if (bindSock < 0) {
+        printf("bind nie powiodl sie");
+        return 1;
+    }
 
     listen(sdServerSocket, 10);
 
     sin_size = sizeof(struct sockaddr_in);
 
-    while(1) {
 
-        while ((sdConnection = accept(sdServerSocket, (struct sockaddr *) &incoming, &sin_size) > 0)) {
-            printf("POLACZENIE Z %s:%d\n", inet_ntoa(incoming.sin_addr), ntohs(incoming.sin_port));
+    while ((sdConnection = accept(sdServerSocket, (struct sockaddr *) &incoming, &sin_size))) {
+        if (sdConnection == -1) {
+            continue;
+        }
+        printf("POLACZENIE Z %s:%d\n", inet_ntoa(incoming.sin_addr), ntohs(incoming.sin_port));
 
-            if (recv(sdConnection, &buf, 1024, 0) < 0) { printf("recv sie nie powiodl\n"); }
+        counter = 1024;
 
+        char *pointer = buf;
+        int capacity = 12;
+
+        while (capacity > 0) {
+            received = recv(sdConnection, pointer, capacity, 0);
+
+            if (received < 0) {
+                perror("recv sie nie powiodl\n");
+                break;
+            }
             printf("odebrano: %s", buf);
 
+            capacity -= received;
+            pointer += received;
+            /*
+            if (send(sdConnection, &buf, 1024, 0) < 0) { printf("send sie nie powiodl\n"); }
+
             close(sdConnection);
-
-
+             */
         }
-
+        close(sdConnection);
     }
+
+    printf("\n%s\n", buf);
+
+
     return 0;
 
 }
