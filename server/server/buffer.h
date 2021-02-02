c06fa017076af7ea2addf62d617aff0ed19141ee
SaluSL <47835218+SaluSL@users.noreply.github.com>
2021-02-02 23:40:11 +0100

    Update buffer.h

--- server/server/buffer.h
+++ server/server/buffer.h
@@ -16,6 +16,7 @@ void free_buffer(pkt_buffer_t *buffer);
 void write_to_buffer(pkt_buffer_t *buffer, void *data, int size);
 int buffer_available_data(pkt_buffer_t *buffer);
 int buffer_remaining_space(pkt_buffer_t *buffer);
+int buffer_size(pkt_buffer_t *buffer);
 void buffer_clear(pkt_buffer_t *buffer);
 pkt_buffer_t *buffer_clone(pkt_buffer_t *buffer);
 
